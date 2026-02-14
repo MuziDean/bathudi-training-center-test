b9eceaabd7122040a16fbbe46fda6fc583a1714b
MuziDean <muzidean14@gmail.com>
2026-02-14 22:28:40 +0200

    Add new homepage background images: 13, 16, 17, 18, 32

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -41,7 +41,7 @@ const Home: React.FC<HomeProps> = ({ onNavigate, onViewNews }) => {
   const [bgImageIdx, setBgImageIdx] = useState(0);
   const [directorData, setDirectorData] = useState<any>(null);
   
-  const bgImages = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '13.jpg', '14.jpg', '7.jpg', '8.jpg', '9.jpg', '16.jpg','17.jpg','18.jpg','32.jpg'];
+  const bgImages = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '13.jpeg', '14.jpeg', '7.jpg', '8.jpg', '9.jpg', '16.jpeg','17.jpeg','18.jpeg','32.jpeg'];
 
   useEffect(() => {
     const fetchData = async () => {
